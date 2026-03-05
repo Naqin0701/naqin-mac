0dec4603305cc7665ed596a55a6396dcaf565af1
Naqin <naqin0701@gmail.com>
2026-03-05 11:42:10 +0800

    Add: zsh plugins

---
+++ zsh/.zsh/zsh-plugins.zsh
@@ -0,0 +1,8 @@
+# ZSH AUTOSUGGESTIONS
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+# ZSH SYNTAX HIGHLIGHTING
+source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# ZSH COMPLETION
+fpath=(~/.zsh/zsh-completion/src $fpath)
