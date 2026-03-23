d1a7c73e53b8aba8ff6cb01e999c2eb8ee1580a7
Naqin <naqin0701@gmail.com>
2026-03-23 23:38:51 +0800

    feat(space): Apply Rose Pine colors with floating card effect

--- sketchybar/.config/sketchybar/plugins/space.sh
+++ sketchybar/.config/sketchybar/plugins/space.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
-# The $SELECTED variable is available for space components and indicates if
-# the space invoking this script (with name: $NAME) is currently selected:
-# https://felixkratz.github.io/SketchyBar/config/components#space----associate-mission-control-spaces-with-an-item
+# Rose Pine 配色工作区脚本
+# $SELECTED: 是否为当前选中 space
+# $NAME:    当前 space 的名称
 
-sketchybar --set "$NAME" background.drawing="$SELECTED"
+CARD_BG_SELECTED="0x66c4a7e7"  # 选中：40% 不透明紫罗兰
+CARD_BG_INACTIVE="0xff232136"   # 未选中：卡片背景
+ICON_COLOR_SELECTED="0xffe0def4"  # 选中：主文字色
+ICON_COLOR_INACTIVE="0xff6e6a86"  # 未选中：淡化文字色
+
+if [ "$SELECTED" = "true" ]; then
+  sketchybar --set "$NAME" \
+    background.color="$CARD_BG_SELECTED" \
+    icon.color="$ICON_COLOR_SELECTED"
+else
+  sketchybar --set "$NAME" \
+    background.color="$CARD_BG_INACTIVE" \
+    icon.color="$ICON_COLOR_INACTIVE"
+fi
